86af651423a111a356fdf39aec24e1f173f1c035
vladwizard <v290801@gmail.com>
2022-11-20 00:40:19 +0300

    -

--- src/TaskBlock/TaskBlock.tsx
+++ src/TaskBlock/TaskBlock.tsx
@@ -8,51 +8,50 @@ import {Task} from "../Types"
  */
 
 export default function ({task, tasks, remove}: { task: Task, tasks: Task[], remove: () => void }) {
-    const title = React.useState(task.title);
-    const description = React.useState(task.description);
-    const file = React.useState(task.file);
-    const date = React.useState(task.endDate);
-    const complete = React.useState(task.complete);
+    const [title,setTitle] = React.useState(task.title);
+    const [description,setDescription] = React.useState(task.description);
+    const [file, setFile] = React.useState(task.file);
+    const [endDate, setEndDate] = React.useState(task.endDate);
+    const [complete,setComplete] = React.useState(task.complete);
 
     useEffect(() => {
-        title[1](task.title)
-        description[1](task.description)
-        file[1](task.file)
-        date[1](task.endDate)
-        complete[1](task.complete)
+        setTitle(task.title)
+        setDescription(task.description)
+        setFile(task.file)
+        setEndDate(task.endDate)
+        setComplete(task.complete)
     }, [tasks])
 
     return (
         <div className="TaskBlock flexColumn">
             <input className='title' onChange={e => {
-                title[1](e.target.value)
+                setTitle(e.target.value)
                 task.title = e.target.value
-            }} value={title[0]}/>
+            }} value={title}/>
             <textarea className='description' onChange={e => {
-                description[1](e.target.value)
+                setDescription(e.target.value)
                 task.description = e.target.value
-            }} value={description[0]}/>
+            }} value={description}/>
             <div className={"flexRow"}>
                 <input type="file" onChange={e => {
-                    file[1](e.target.value)
+                    setFile(e.target.value)
                     task.file = e.target.value
-                }} value={file[0]}/>
-                <input onChange={e => {
-                    date[1](e.target.value)
+                }} value={file}/>
+                <input type="date" onChange={e => {
+                    setEndDate(e.target.value)
                     task.endDate = e.target.value
-                }} type="date"/>
+                }} value={endDate}/>
                 <div className="completeArea">
                     <label htmlFor="complete">Завершена:</label>
-                    <input id="complete" type="checkbox" checked={complete[0]}
+                    <input id="complete" type="checkbox" checked={complete}
                            onChange={e => {
-                               complete[1](e.target.checked)
+                               setComplete(e.target.checked)
                                task.complete = e.target.checked
                            }}/>
                 </div>
-                <button onClick={(e) => remove()
-                }>Удалить
+                <button onClick={e => remove()}>Удалить
                 </button>
-                {new Date(date[0]) < new Date() ? <p>Время кончилось</p> : ''}
+                {new Date(endDate) < new Date() ? <p>Время кончилось</p> : ''}
             </div>
         </div>
     );
